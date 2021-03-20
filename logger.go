3091537e035f6321bcf929d0cc20a5de1c38ac88
Zhao KK <kikakkz@hotmail.com>
2021-03-20 10:27:18 +0800

    Support filename in log

--- logger.go
+++ logger.go
@@ -3,6 +3,9 @@ package elog
 import (
 	"github.com/sirupsen/logrus"
 	"os"
+	"runtime"
+	"strconv"
+	"strings"
 )
 
 var log = logrus.New()
@@ -14,20 +17,48 @@ func init() {
 	log.Out = os.Stdout
 }
 
+func logEntry() *logrus.Entry {
+	pc, file, line, ok := runtime.Caller(2)
+	if !ok {
+		return log.WithField("fatal", "cannot get caller")
+	}
+
+	paths := strings.Split(file, "/")
+	var filePath string
+
+	j := 0
+	i := len(paths) - 1
+
+	for j < 2 && 0 <= i {
+		if 0 < len(filePath) {
+			filePath = paths[i] + "/" + filePath
+		} else {
+			filePath = paths[i]
+		}
+		i -= 1
+		j += 1
+	}
+
+	filename := filePath + ":" + strconv.Itoa(line)
+	funcname := runtime.FuncForPC(pc).Name()
+	fn := funcname[strings.LastIndex(funcname, ".")+1:]
+	return log.WithField("file", filename).WithField("func", fn)
+}
+
 func Errorf(fields Fields, fmt string, args ...interface{}) {
-	log.WithFields(fields).Errorf(fmt, args...)
+	logEntry().WithFields(fields).Errorf(fmt, args...)
 }
 
 func Fatalf(fields Fields, fmt string, args ...interface{}) {
-	log.WithFields(fields).Fatalf(fmt, args...)
+	logEntry().WithFields(fields).Fatalf(fmt, args...)
 }
 
 func Infof(fields Fields, fmt string, args ...interface{}) {
-	log.WithFields(fields).Infof(fmt, args...)
+	logEntry().WithFields(fields).Infof(fmt, args...)
 }
 
 func Debugf(fields Fields, fmt string, args ...interface{}) {
-	log.WithFields(fields).Debugf(fmt, args...)
+	logEntry().WithFields(fields).Debugf(fmt, args...)
 }
 
 func SetLevel(levelStr string) error {
